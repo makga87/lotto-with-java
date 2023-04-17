d6d2249384e5179b13a2bb4edfd1610681593589
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-04-17 21:48:10 +0900

    [ADD] 로또 판매점 객체 팩터리메서드 추가

--- src/main/java/lotto/domain/LottoShop.java
+++ src/main/java/lotto/domain/LottoShop.java
@@ -1,11 +1,15 @@
-package lotto.service;
-
-import lotto.domain.Lottos;
-import lotto.domain.NumbersGenerable;
+package lotto.domain;
 
 public class LottoShop {
 
 	private int LOTTO_PRICE = 1000;
+
+	private LottoShop() {
+	}
+
+	public static LottoShop init() {
+		return new LottoShop();
+	}
 
 	public Lottos sell(int money, NumbersGenerable numbersGenerable) {
 		return Lottos.of(money / LOTTO_PRICE, numbersGenerable);
