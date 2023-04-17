97473bdac53c17867a046e65bdf59932c191d660
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-04-17 11:51:12 +0900

    [ADD] 테스트 코드에 한글 설명 추가

--- src/test/java/lotto/NumberTest.java
+++ src/test/java/lotto/NumberTest.java
@@ -2,6 +2,7 @@ package lotto;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -10,6 +11,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class NumberTest {
 
+	@DisplayName("로또에서 지원하지 않는 번호가 입력되는 경우, 예외가 발생한다")
     @ParameterizedTest
     @ValueSource(ints = {0, 47})
     void testNumberValidation(int exceptionNumber) {
@@ -19,6 +21,7 @@ class NumberTest {
                 .hasMessageContaining("Invalid number input");
     }
 
+	@DisplayName("동일한 번호의 객체는, 동치성이 성립한다")
     @Test
     void testEqualsNumber(){
        assertTrue(Number.from(1).equals(Number.from(1)));
