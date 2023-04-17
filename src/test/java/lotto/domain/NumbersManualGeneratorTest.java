3896150e1e17cb466a04286836e69c9daf18042d
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-04-17 22:03:00 +0900

    [ADD] 설명 추가

--- src/test/java/lotto/domain/NumbersManualGeneratorTest.java
+++ src/test/java/lotto/domain/NumbersManualGeneratorTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
@@ -11,6 +12,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 class NumbersManualGeneratorTest {
 
 
+	@DisplayName("숫자가 아닌 값이 들어오면, 예외를 발생시킨다")
 	@ParameterizedTest
 	@CsvSource(value = {
 			"1, 2, 3, 4, 5, ",
