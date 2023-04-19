3288064baab7ae2fff24c93064a8e6d925a12f1e
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-04-19 21:32:45 +0900

    [ADD] View 개발

--- src/main/java/lotto/view/InputView.java
+++ src/main/java/lotto/view/InputView.java
@@ -8,12 +8,17 @@ public class InputView {
 
 	private static int LOTTO_PRICE = 1000;
 
-	public static int buyLotto() {
+	public static int inputMoney() {
 		System.out.println("구입금액을 입력해 주세요.");
 		String money = scanner.next();
 		System.out.println(money);
 		int count = Integer.parseInt(money) / LOTTO_PRICE;
 		System.out.println(count + "개를 구매했습니다.");
 		return count;
+	}
+
+	public static String inputWinningNumber() {
+		System.out.println("지난 주 당첨 번호를 입력해 주세요.");
+		return scanner.next();
 	}
 }
--- src/main/java/lotto/view/ResultView.java
+++ src/main/java/lotto/view/ResultView.java
@@ -1,15 +1,37 @@
 package lotto.view;
 
+import java.util.Map;
 import java.util.Scanner;
 
 import lotto.domain.LottoReturns;
+import lotto.domain.Lottos;
+import lotto.domain.WinningsTable;
 
 public class ResultView {
 
 	private static Scanner scanner = new Scanner(System.in);
 
+	public static void printBoughtLotto(Lottos lottos) {
+		lottos.getLottos()
+			  .forEach(lotto -> {
+				  System.out.println(lotto.toString());
+			  });
+	}
 
-	public static void print(LottoReturns lottoReturns) {
+	public static void printLottoReturns(LottoReturns lottoReturns) {
 
+		Map<WinningsTable, Integer> lottoReturnsStatus = lottoReturns.getLottoReturnsStatus();
+		lottoReturnsStatus.entrySet()
+						  .stream()
+						  .forEach(entry -> {
+							  WinningsTable winningsTable = entry.getKey();
+							  int matchingCount = entry.getValue();
+
+							  String status = String.format("%d개 일치 (%d원) - %d", winningsTable.getMatchingCount(), winningsTable.getMoney(),
+															matchingCount);
+							  System.out.println(status);
+						  });
+
+		System.out.println(String.format("총 수익률은 %.2f입니다.", lottoReturns.getReturns()));
 	}
 }
