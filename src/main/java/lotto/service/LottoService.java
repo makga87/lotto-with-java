94aae4be16f8f2987ed7661f7baea8179e23648a
Kim HyungGeun <hyunggeun.kim@bugs.com>
2023-04-19 21:33:26 +0900

    [ADD] 기능별 서비스 로직 개발

--- src/main/java/lotto/service/LottoService.java
+++ src/main/java/lotto/service/LottoService.java
@@ -1,20 +1,44 @@
 package lotto.service;
 
 import lotto.domain.Lotto;
+import lotto.domain.LottoReturns;
 import lotto.domain.LottoShop;
 import lotto.domain.Lottos;
 import lotto.domain.NumbersAutoGenerator;
+import lotto.domain.NumbersManualGenerator;
+import lotto.domain.NumbersMatchingCounter;
+import lotto.domain.WinningsTable;
+import lotto.view.InputView;
 
 public class LottoService {
 
-	public void execute() {
+	private LottoService() {
+	}
 
-		int money = 14000;
+	public static LottoService init() {
+		return new LottoService();
+	}
 
+	public Lottos buyLotto(int money) {
 		LottoShop lottoShop = LottoShop.init();
+		return lottoShop.sell(money, NumbersAutoGenerator.init());
+	}
 
-		Lottos myLottos = lottoShop.sell(money, NumbersAutoGenerator.init());
+	public Lotto checkWinningLotto() {
+		return Lotto.generate(NumbersManualGenerator.from(InputView.inputWinningNumber()));
+	}
 
+	public LottoReturns compareWithMyLotto(Lottos myLottos, Lotto winningLotto, int money) {
+		LottoReturns lottoReturns = LottoReturns.from(money);
 
+		myLottos.getLottos()
+				.forEach(lotto -> {
+					NumbersMatchingCounter numbersMatchingCounter = NumbersMatchingCounter.init();
+					int matchCount = numbersMatchingCounter.count(lotto.getNumbers(), winningLotto.getNumbers());
+					WinningsTable winningsTable = WinningsTable.getByMatchingCount(matchCount);
+					lottoReturns.countPlusReturns(winningsTable);
+				});
+
+		return lottoReturns;
 	}
 }
